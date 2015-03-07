165bf2e90be06d3489c06781e23056091b3c7d71
Naomi Leow <naomileowwx@gmail.com>
2015-03-07 19:24:36 +0100

    added google api wrapper for angular

--- www/js/modules/calendar/calendar.js
+++ www/js/modules/calendar/calendar.js
@@ -1,6 +1,7 @@
 angular.module('googleCal')
 .constant('apiKey', '598ba433f7e26a717d5485574223b8bef0bce73b')
-.constant('clientId', '892332260770-oq10pm310ql31ug3gd0fq23sq9slc658.apps.googleusercontent.com')
-.controller('calendarController', function($scope){
+.constant('clientId', '892332260770-oq10pm310ql31ug3gd0fq23sq9slc658.apps.googleusercontent.com');
 
-});
+function getCalendar($scope){
+	
+}
