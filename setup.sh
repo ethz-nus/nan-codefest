bc18207e55becb3c0f6578c4a4e30030ff5290a3
jellyjellyrobot <jellyjellyrobot@gmail.com>
2015-03-07 12:46:42 +0100

    added setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+apt-get update
+apt-get upgrade
+apt-get install zsh git
+
+chsh -s /usr/bin/zsh
+
+#Install oh-my-zsh
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sudo sh
