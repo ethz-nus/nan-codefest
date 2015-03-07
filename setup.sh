9e07e3cda838d324863fd45d7269541ca332feec
jellyjellyrobot <jellyjellyrobot@gmail.com>
2015-03-07 13:49:34 +0100

    add ionic/cordova folders to gitignore

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 apt-get update
 apt-get upgrade
-apt-get install zsh git nodejs
+apt-get install zsh git nodejs npm
 
 chsh -s /usr/bin/zsh
 
 #Install oh-my-zsh
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sudo sh
+
+npm install -g cordova ionic
