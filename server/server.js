ef311e2ea3e5976ae4f4ca6deb254b15cdf46d18
jellyjellyrobot <jellyjellyrobot@gmail.com>
2015-03-08 07:29:58 +0100

    hotfix++

--- server/server.js
+++ server/server.js
@@ -89,8 +89,8 @@ var app = express();
 
 //CORS middleware
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'example.com');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 }
