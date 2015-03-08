8cf05377623fe48f50a738f76af2fd8e212b1647
jellyjellyrobot <jellyjellyrobot@gmail.com>
2015-03-08 07:57:05 +0100

    cors try #2

--- server/server.js
+++ server/server.js
@@ -85,15 +85,19 @@ mongoose.connect(mongoUri, function(err){
 Init Socket.io
 */
 
+
+
+
 var app = express();
 
 //CORS middleware
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept');
     next();
-}
+};
+
 app.use(allowCrossDomain);
 
 var ioServer = require('http').Server(app)
