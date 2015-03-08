fbf45967a08f2d3a8ad5124ad4f2d9f4c166088d
jellyjellyrobot <jellyjellyrobot@gmail.com>
2015-03-08 05:51:33 +0100

    allow CORS

--- server/server.js
+++ server/server.js
@@ -86,6 +86,20 @@ Init Socket.io
 */
 
 var app = express();
+
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
+//...
+app.configure(function() {;
+    app.use(allowCrossDomain);
+});
+
 var ioServer = require('http').Server(app)
 var io = require('socket.io')(ioServer);
 
